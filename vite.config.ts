bb5b0f6c0828b86ab742311bcd6f81e4fde28cb5
simrankhurmi <simrankhurmi121@gmail.com>
2025-05-14 19:37:40 +0530

    vite config file changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
    base: '/portfolio_replica_figma/',
-  plugins: [react() , tailwindcss()],
+  plugins: [react()],
 })

17da20667a55349eb023aaaee7e4440f4cd272b8
simrankhurmi <simrankhurmi121@gmail.com>
2025-05-21 19:45:47 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/',
+   base: '/portfolio_replica_figma/',
   plugins: [react() , tailwindcss()],
 })
